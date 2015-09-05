0d7d3d25d03b397dfa02bc184bc2953d3a8ff92b
Ralph Viktor <ralphviktor@gmail.com>
2015-09-05 15:35:59 +0200

    Added Related anime

--- MAL.Net/Classes/AnimeRetriever.cs
+++ MAL.Net/Classes/AnimeRetriever.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using System.Web.UI.WebControls;
 using HtmlAgilityPack;
 using MAL.Net.Objects;
 
@@ -13,6 +14,7 @@ namespace MAL.Net.Classes
         #region Variables
 
         private const string MalUrl = @"http://myanimelist.net/anime/{0}";
+        private const string CleanMalUrl = @"http://myanimelist.net{0}";
         private readonly string _examplePath = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
 
         #endregion
@@ -104,6 +106,8 @@ namespace MAL.Net.Classes
                             break;
                     }
                 }
+                GetInfoUrls(doc, anime);
+                GetRelated(doc, anime);
 
             }
             catch (Exception ex)
@@ -114,5 +118,119 @@ namespace MAL.Net.Classes
 
             return anime;
         }
+
+        #region Private Methods
+
+        private void GetInfoUrls(HtmlDocument doc, Anime anime)
+        {
+            foreach (var listItem in doc.DocumentNode.SelectNodes("//div[@id='horiznav_nav']"))
+            {
+                foreach(var child in listItem.ChildNodes["ul"].ChildNodes)
+                {
+                    var item = child.ChildNodes["a"];
+                    if(item == null) continue;
+                    switch (item.InnerText)
+                    {
+                        case "Episodes":
+                            anime.AdditionalInfoUrls.Episodes = child.ChildNodes["a"].Attributes["href"].Value;
+                            break;
+                        case "Reviews":
+                            anime.AdditionalInfoUrls.Reviews = child.ChildNodes["a"].Attributes["href"].Value;
+                            break;
+                        case "Recommendations":
+                            anime.AdditionalInfoUrls.Recommendation = child.ChildNodes["a"].Attributes["href"].Value;
+                            break;
+                        case "Stats":
+                            anime.AdditionalInfoUrls.Stats = child.ChildNodes["a"].Attributes["href"].Value;
+                            break;
+                        case "Characters &amp; Staff":
+                            anime.AdditionalInfoUrls.CharactersAndStaff = child.ChildNodes["a"].Attributes["href"].Value;
+                            break;
+                        case "News":
+                            anime.AdditionalInfoUrls.News = child.ChildNodes["a"].Attributes["href"].Value;
+                            break;
+                        case "Forum":
+                            anime.AdditionalInfoUrls.Forum = child.ChildNodes["a"].Attributes["href"].Value;
+                            break;
+                        case "Featured":
+                            anime.AdditionalInfoUrls.Featured = child.ChildNodes["a"].Attributes["href"].Value;
+                            break;
+                        case "Clubs":
+                            anime.AdditionalInfoUrls.Clubs = child.ChildNodes["a"].Attributes["href"].Value;
+                            break;
+                        case "Pictures":
+                            anime.AdditionalInfoUrls.Pictures = child.ChildNodes["a"].Attributes["href"].Value;
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void GetRelated(HtmlDocument doc, Anime anime)
+        {
+            var node = doc.DocumentNode.SelectSingleNode("//table[@class='anime_detail_related_anime']").ChildNodes["tr"];
+            ParseTd(node, anime);
+        }
+
+        private void ParseTd(HtmlNode node, Anime anime)
+        {
+            switch (node.ChildNodes[0].InnerText.Replace(":",""))
+            {
+                case "Adaptation":
+                    anime.MangaAdaptation.Add(MapRelated(node));
+                    break;
+                case "Prequel":
+                    anime.Prequels.Add(MapRelated(node));
+                    break;
+                case "Sequel":
+                    anime.Sequels.Add(MapRelated(node));
+                    break;
+                case "Side Story":
+                    anime.SideStories.Add(MapRelated(node));
+                    break;
+                case "Parent Story":
+                    anime.ParentStory = MapRelated(node);
+                    break;
+                case "Character Anime":
+                    anime.CharacterAnime.Add(MapRelated(node));
+                    break;
+                case "Spin Off":
+                    anime.SpinOffs.Add(MapRelated(node));
+                    break;
+                case "Summary":
+                    anime.Summaries.Add(MapRelated(node));
+                    break;
+                case "Alternative Versions":
+                    anime.AlternativeVersion.Add(MapRelated(node));
+                    break;
+                default:
+                    anime.Others.Add(MapRelated(node));
+                    break;
+
+            }
+            if (node.ChildNodes.Count == 3)
+            {
+                var nextNode = node.ChildNodes[2];
+                if (nextNode != null)
+                    ParseTd(nextNode, anime);
+            }
+        }
+
+        private Related MapRelated(HtmlNode node)
+        {
+            var subNode = node.ChildNodes[1];
+            var url = subNode.ChildNodes["a"].Attributes["href"].Value;
+            var related = new Related
+            {
+                Title = subNode.InnerText,
+                Url = string.Format(CleanMalUrl, url)
+            };
+            var parts = url.Split('/');
+            int id;
+            int.TryParse(parts[2], out id);
+            related.Id = id;
+            return related;
+        }
+        #endregion
     }
 }
